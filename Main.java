f0887de0c4e178c7537b742caa401e80f2f29e37
thollister74 <49489585+thollister74@users.noreply.github.com>
2019-05-02 16:11:42 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+package src;
+//Created by hollisti on 2019-05-02.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String fName = "Sterling";
+        String lName = "Archer";
+        int userAge = 34;
+
+        System.out.println ("Your first name: "+fName);
+        System.out.println("Your first name: "+lName);
+        System.out.println(("You are ") + userAge +( " years old"));
+        System.out.println("Are you from Michigan? Y/N");
+        Scanner scan = new Scanner(System.in);
+
+        String fromMichigan = scan.nextLine();
+        switch (fromMichigan) {
+            case "Y":
+            case "y":
+                System.out.println("Glad you're not a Buckeye");
+
+                break;
+            case "N":
+            case "n":
+                System.out.println("Ew, I hope your not from Ohio.");
+
+                break;
+            default:
+                System.out.println("Dude?! 'Y' or 'N'! It's not that hard! \n Press ENTER and try again!");
+
+                break;
+        }
+// This is the if-else statement I created, the Switch statement above was set up by Intellij
+//*****  String fromMichigan = scan.nextLine();
+//        if (fromMichigan.equals("Y") || fromMichigan.equals("y")) {
+//            System.out.println("Glad you're not a Buckeye");
+//
+//        } else if (fromMichigan.equals("N") || fromMichigan.equals("n")) {
+//            System.out.println("Ew, I hope your not from Ohio.");
+//
+//        } else {
+//            System.out.println("Dude?! 'Y' or 'N'! It's not that hard!");
+//
+//*****   }
+
+
+    }
+}
---
+++ Runner.java
@@ -0,0 +1,15 @@
+
+// Created by hollisti on 2019-05-02.
+
+import java.util.Scanner;
+
+public class Runner {
+
+    String fName;
+    String lName;
+    int age = 34;
+    boolean isMichiganResident;
+
+
+
+}
